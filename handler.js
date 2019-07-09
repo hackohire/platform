04e861bb728a2b8395c407022f677a8e01c21662
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-07-09 09:50:32 +0530

    Mongo connection directly from the handler

--- handler.js
+++ handler.js
@@ -1,27 +1,29 @@
 const {ApolloServer} = require('apollo-server-lambda');
-
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const connectToMongoDB = require('./helpers/db');
+const auth = require('./helpers/auth');
+
 
 const server = new ApolloServer({
   cors: true,
   typeDefs,
   resolvers,
-  context: ({event, context}) => ({
+  context: async ({event, context}) => ({
     headers: event.headers,
     functionName: context.functionName,
     event,
     context,
+    decodedToken: auth.auth(event.headers),
+    db: await connectToMongoDB() 
   }),
   introspection: true,
   playground: true,
 });
-
-// exports.graphql = server.createHandler();
 
 module.exports.graphqlHandler = server.createHandler({
   cors: {
     origin: '*',
     credentials: true,
   },
-});
+}, );
