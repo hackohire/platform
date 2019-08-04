f186cc66b67cc5ddb8f668f03dba871f4ce5eedc
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-08-04 20:18:20 +0530

    ProductCreationNotificationToAdmin added

--- helpers/ses_createTemplate.js
+++ helpers/ses_createTemplate.js
@@ -6,13 +6,31 @@ AWS.config.update({
     secretAccessKey: process.env.AWS_SECRETKEY,
   });
 
-const ses = new AWS.SES(); 
+const ses = new AWS.SES();
 
-const params = {
+const AppCreationNotificationToAdmin = {
   "Template": {
     "TemplateName": "AppCreationNotificationToAdmin",
     "SubjectPart": "Application Creation Notification",
     "HtmlPart": "<h1>Hello {{name}},</h1><p>An application {{appName}} has been created. Please Click here {{link}} to check the details.</p>",
+    "TextPart": ""
+  }
+}
+
+const ProductCreationNotificationToAdmin = {
+  "Template": {
+    "TemplateName": "ProductCreationNotificationToAdmin",
+    "SubjectPart": "Product Creation Notification",
+    "HtmlPart": "<h1>Hello {{name}},</h1><p>A product {{productName}} has been created. Please Click here {{link}} to check the details.</p>{{productDetails}}<p>",
+    "TextPart": ""
+  }
+}
+
+const params = {
+  "Template": {
+    "TemplateName": "ProductCreationNotificationToAdmin",
+    "SubjectPart": "Product Creation Notification",
+    "HtmlPart": "<h1>Hello {{name}},</h1><p>A product {{productName}} has been created. Please Click here {{link}} to check the details.</p>{{productDetails}}<p>",
     "TextPart": ""
   }
 }
